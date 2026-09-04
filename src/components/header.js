59336b12091dbba2d1cbeb660308ef68b3133c7a
mohammed <m@gmail.com>
2026-09-04 15:40:21 +0300

    a

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+import  "../scss/variables.scss";
+
+export function header(){
+    return `
+    <header class="">
+        <nav>
+            <ul>
+                <li>Home</li>
+            </ul>
+        </nav>
+    </header>`
+} 
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { header } from "./components/header";
+
+const app = document.getElementById("app");
+
+// إدراج محتوى الهيدر داخل عنصر app
+app.innerHTML = header();
+
